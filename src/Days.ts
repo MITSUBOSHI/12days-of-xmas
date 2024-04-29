b4287c50a047396235c6011d51d8bddb2e2915ff
mitsubosh <yuya.mitsuboshi@gmail.com>
2024-04-29 22:14:01 +0900

    Refactor/rename Days.ts

--- src/Days.ts
+++ src/Days.ts
@@ -12,14 +12,6 @@ import Day10Image from './assets/12days_of_xmas/day10.png';
 import Day11Image from './assets/12days_of_xmas/day11.png';
 import Day12Image from './assets/12days_of_xmas/day12.png';
 
-const days = Array.from(Array(12).keys(), x => x + 1);
-type Day = (typeof days)[number];
-type DayType = {
-  dayNumber: Day;
-  nameEn: string,
-  nameJa: string,
-  image: string
-};
 const images = {
   1: Day1Image,
   2: Day2Image,
@@ -34,8 +26,17 @@ const images = {
   11: Day11Image,
   12: Day12Image,
 };
+type imagesType = typeof images;
+type DayNumberType = keyof imagesType;
+type DayType = {
+  dayNumber: DayNumberType;
+  nameEn: string,
+  nameJa: string,
+  image: string
+};
+type DayDataType = Omit<DayType, 'image'>;
 
-export const Days: DayType[] = dayData.map((day) => {
+export const Days: DayType[] = (dayData as DayDataType[]).map((day) => {
   return {
     dayNumber: day.dayNumber,
     nameEn: day.nameEn,
