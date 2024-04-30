26e5bc64bc588e444075040665a514f87e1d0de5
mitsubosh <yuya.mitsuboshi@gmail.com>
2024-05-01 00:43:36 +0900

    fix settings for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_PAGES
+    ? "12days-of-xmas"
+    : "./",
   server: {
     port: 8080,
     strictPort: true,
